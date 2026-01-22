be825c28968cb3316daefd1f98e48b8835d8284b
zaid-afridi <zaidafridi6868@gmail.com>
2026-01-22 15:47:08 +0500

    Add task

---
+++ taxk1.js
@@ -0,0 +1,11 @@
+let inputA = document.getElementById('inputA')
+let inputB = document.getElementById('inputB')
+let button1 = document.getElementById('button1')
+
+function answer() {
+
+
+}
+
+
+button1.addEventListener('click', answer)
