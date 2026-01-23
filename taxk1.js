9d46e7056b1c544118e955c125ef7f1db681ab27
zaid-afridi <zaidafridi6868@gmail.com>
2026-01-23 11:04:08 +0500

    Add taxk1

--- taxk1.js
+++ taxk1.js
@@ -1,11 +1,17 @@
-let inputA = document.getElementById('inputA')
-let inputB = document.getElementById('inputB')
 let button1 = document.getElementById('button1')
+let result = document.getElementById('answer')
+
+let sum;
 
 function answer() {
+    const a = document.getElementById('inputA').value
 
+    const inputB = document.getElementById('inputB').value
 
+    sum = Number(a) + Number(inputB)
+    console.log('sum', sum);
+
+    result.innerText = sum
 }
-
 
 button1.addEventListener('click', answer)
